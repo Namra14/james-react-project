41e1f8ee243245aca77c18b92da8f234f6159ab3
James <131582155+Namra14@users.noreply.github.com>
2024-07-07 19:57:42 +0800

    dsadsa

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,6 +1,5 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import myProfile from './img/Grad-Pic-1.jpg'
 import './App.css'
 import './styles.scss'
 
@@ -8,28 +7,37 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className='card'>
+          <img src={myProfile} className="picture" alt="Vite logo" />
+      <h1 className='name'>James Angeles</h1>
+      <p className='des'>Front-End Developer</p>
+      <a href="https://namra14.github.io/JamesRolandAngeles-Portfolio/" className='link' target='_blank'>MyProfolio</a>
+        <div className='button'> 
+          <a href="">
+            
+            <button> <i class="bi bi-envelope-at-fill"></i>Email </button>
+          </a>
+          <a href="">
+            <button className='btn2'><i class="bi bi-linkedin"></i>LinkedIn</button>
+          </a>
+        </div>
+
+        <div className='about'>
+          <h3>About</h3>
+          <p className='des1'>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            Tempore aliquid deserunt magni ipsa nihil tempora sint voluptates temporibus, atque, 
+            odit commodi. Eum libero laborum itaque necessitatibus quo a saepe mollitia?</p>
+        </div>
+
+        <div className='int'>
+          <h3>Interest</h3>
+          <p className='des2'>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            Tempore aliquid deserunt magni ipsa nihil tempora sint voluptates temporibus, atque, 
+            odit commodi. Eum libero laborum itaque necessitatibus quo a saepe mollitia?</p>
+        </div>
+        
+       
       </div>
-      <h1>James Learning Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
   )
 }
 
